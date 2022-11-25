9e39232b5d837011c895863b629cc7cf115cda48
FARIZ <farizmuh25@gmail.com>
2022-11-25 15:04:33 +0700

    coba

--- app/src/main/java/com/ngemeal/ngemeal/ui/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/com/ngemeal/ngemeal/ui/splashscreen/SplashScreenActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import com.ngemeal.ngemeal.MainActivity
 import com.ngemeal.ngemeal.R
 import com.ngemeal.ngemeal.ui.auth.AuthActivity
 
